6bcf3bae51c56b2e42e06f8dd9b47ebb04ef86ae
Matthew Parisien <matthewparisien4@gmail.com>
2021-08-19 20:03:09 +0000

    Removing code from a previous ejs tutorial exercise. No longer needed for tinyapp.

--- express_server.js
+++ express_server.js
@@ -4,24 +4,10 @@ const PORT = 8080;
 
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-  const mascots = [
-    { name: 'Sammy', organization: "DigitalOcean", birth_year: 2012},
-    { name: 'Tux', organization: "Linux", birth_year: 1996},
-    { name: 'Moby Dock', organization: "Docker", birth_year: 2013}
-  ];
-  const tagline = "No programming concept is complete without a cute animal mascot.";
-
-  res.render('pages/index', { //when we use res.render we first provide it with the name of the template, and we also pass along an object
-    mascots: mascots,
-    tagline: tagline
-  });
-});
-
-//about page 
-app.get('/about', (req, res) => {
-  res.render('pages/about');
-});
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`)
