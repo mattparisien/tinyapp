bdf32c179e8f93b1edebb05ba56b90b63fff34ac
Matthew Parisien <matthewparisien4@gmail.com>
2021-08-19 18:14:58 +0000

    Adding an additional server method, which gets the /hello path and responds with a brief HTML document.

--- express_server.js
+++ express_server.js
@@ -13,8 +13,13 @@ app.get('/', (req, res) => { // '/' is called the root path
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
+}); 
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 app.listen(PORT, () => {
   console.log(`App is listening on port ${PORT}!`);
 });
+
