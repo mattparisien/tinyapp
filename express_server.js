89de75b9bdefcc0f98a7b4eacf24784705a3ff6f
Matthew Parisien <matthewparisien4@gmail.com>
2021-08-19 19:23:26 +0000

    Modifying the code inside of the get index request. I've added an array of mascots and a tagline, and included this in the render object. This means that I will now be able to reference these variables inside of my index.ejs file.

--- express_server.js
+++ express_server.js
@@ -8,7 +8,17 @@ app.set('view engine', 'ejs');
 
 //use res.render to load up an ejs view file//index page 
 app.get('/', (req, res) => {
-  res.render('pages/index');
+  var mascots = [
+    { name: 'Sammy', organization: "DigitalOcean", birth_year: 2012},
+    { name: 'Tux', organization: "Linux", birth_year: 1996},
+    { name: 'Moby Dock', organization: "Docker", birth_year: 2013}
+  ];
+  var tagline = "No programming concept is complete without a cute animal mascot.";
+
+  res.render('pages/index', {
+    mascots: mascots,
+    tagline: tagline
+  });
 });
 
 //about page 
