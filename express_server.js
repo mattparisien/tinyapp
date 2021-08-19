9d0d04aa6ba613e1951df5e2cc34677de4ebae8f
Matthew Parisien <matthewparisien4@gmail.com>
2021-08-19 18:38:57 +0000

    Setting ejs as the view engine for JS, and rendering each html page using the render method on the response object.

--- express_server.js
+++ express_server.js
@@ -2,24 +2,44 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
-const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
-};
+//ejs: Embedded Javascript Templates
+//set the view engine for the express app to ejs
+app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => { // '/' is called the root path
-  res.send('hey!')
+//use res.render to load up an ejs view file//index page 
+app.get('./', (req, res) => {
+  res.render('pages/index');
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-}); 
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+//about page 
+app.get('/about', (req, res) => {
+  res.render('pages/about');
 });
 
 app.listen(PORT, () => {
-  console.log(`App is listening on port ${PORT}!`);
-});
+  console.log(`Server is listening on port ${PORT}`)
+})
+
+
+
+// const urlDatabase = {
+//   "b2xVn2": "http://www.lighthouselabs.ca",
+//   "9sm5xK": "http://www.google.com"
+// };
+
+// app.get('/', (req, res) => { // '/' is called the root path
+//   res.send('hey!')
+// });
+
+// app.get("/urls.json", (req, res) => {
+//   res.json(urlDatabase);
+// }); 
+
+// app.get("/hello", (req, res) => {
+//   res.send("<html><body>Hello <b>World</b></body></html>\n");
+// });
+
+// app.listen(PORT, () => {
+//   console.log(`App is listening on port ${PORT}!`);
+// });
 
