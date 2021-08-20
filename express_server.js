94cf9b8475da024169cdb225c74f4f39d9b89be5
Matthew Parisien <matthewparisien4@gmail.com>
2021-08-20 13:10:49 +0000

    Adding a new get route to 'urls/new', and rendering our previously create ejs HTML file.

--- express_server.js
+++ express_server.js
@@ -19,10 +19,14 @@ app.get('/urls', (req, res) => {
   res.render('urls_index', templateVars);
 });
 
+app.get('urls/new', (req, res) => {
+  res.render('urls_new');
+})
+
 app.get('/urls/:shortURL', (req, res) => { // ':' indicates that the ID is a route parameter
   const templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]}
   res.render('urls_show', templateVars);
-})
+});
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
