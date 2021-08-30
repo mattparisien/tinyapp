eb02cc401d7b776a34f9862c8742cc2a8573be8f
Matthew Parisien <matthewparisien4@gmail.com>
2021-08-30 15:16:48 +0000

    Add unit test for checking if fetchUserByEmail(object, email) returns an object of the found user.

---
+++ test/helpers.test.js
@@ -0,0 +1,23 @@
+const { assert } = require('chai');
+const { fetchUserByEmail } = require('../helpers');
+
+const testUsers = {
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+};
+
+describe('getUserByEmail', function() {
+  it('should return a user with valid email', function() {
+    const user = fetchUserByEmail(testUsers, "user@example.com")
+    const expectedOutput = testUsers["userRandomID"];
+    assert.equal(user, expectedOutput)
+  });
+});
