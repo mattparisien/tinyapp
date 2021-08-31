d96bf165a6193a8e721d07d28d79b8fa1cd0de0d
Matthew Parisien <matthewparisien4@gmail.com>
2021-08-31 18:49:45 +0000

    Group server logic together.

---
+++ server_setup.js
@@ -0,0 +1,31 @@
+const runServer = function() {
+
+  const express = require('express');
+  const PORT = 8080;
+  const app = express();
+  app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+  });
+
+  //Require dependencies
+  const bodyParser = require('body-parser');
+  const cookieSession = require('cookie-session');
+  const bcrypt = require('bcryptjs');
+
+  //Reference directory from which serving static css file
+  app.use(express.static(__dirname + '/assets')); 
+
+
+  app.use(bodyParser.urlencoded({extended: true}));
+  app.use(cookieSession({
+    name: 'session',
+    keys: ['newesahrdahusdnolsnask'],
+  }))
+
+  app.set('view engine', 'ejs');
+
+  return app;
+
+};
+
+module.exports = { runServer}
