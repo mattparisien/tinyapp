182eabc4319ed2519aaaa355d6d43e821c61636a
Matthew Parisien <matthewparisien4@gmail.com>
2021-08-31 19:26:22 +0000

    Implement further simplification

--- validateForms.js
+++ validateForms.js
@@ -4,36 +4,46 @@ const { fetchUserByEmail, fetchPassword } = require('./helpers');
 
 const validateLogIn = function(object, email, password, req, res, bcrypt) {
   if (!req.body.email || !req.body.password) {
-    res.status(400) 
     const templateVars = { validationError: "Please fill out the fields." } ;
-    res.render('login', templateVars);
+    res.status(400).render('login', templateVars);
+    // return false;
   
     //If no user ID exists, send error
   } else if (!fetchUserByEmail(object, email)) {
-    res.status(400);
-    const templateVars = { validationError: "Email is not registered." } 
-    res.render('login', templateVars);
+    res.redirect(`/login?error=${"Email is not registered."}`);
+    // return false;
   
   //If unhashed, req.body password does not match hashed database password, send error
   } else if (!bcrypt.compareSync(password, fetchPassword(object, fetchUserByEmail(object, email)['id']))) {
-    res.status(400);
     const templateVars = { validationError: "Incorrect password." };
-    res.render('login', templateVars)
+    res.status(400).render('login', templateVars)
+    // return false;
   };
 };
 
-const validateRegister = function(object, email, req, res) {
-//If fields are empty, send error
-if (!req.body.email || !req.body.password) {
-  res.status(400);
-  const templateVars = { validationError: "Please fill out the fields." } 
 
-//If user already exists, send error
-} else if (fetchUserByEmail(object, email) !== undefined) {
-    res.status(400);
-    const templateVars = { validationError: "You already have an account with this email address." } 
-    res.render('registration', templateVars)
-  }
-}
 
-module.exports = { validateLogIn, validateRegister };
+// const validateRegister = function(object, email, req, res) {
+
+  
+
+// //If fields are empty, send error
+// if (!req.body.email || !req.body.password) {
+//   const templateVars = { validationError: "Please fill out the fields." } 
+//   // res.status(400);
+//   return templateVars;
+
+
+// //If user already exists, send error
+// } else if (fetchUserByEmail(object, email) !== undefined) {
+//     const templateVars = "You already have an account with this email address." 
+//     // res.status(400).render('registration', templateVars)
+//     res.redirect('/register');
+//   } else {
+//     object[uniqueId] = new User(uniqueId, email, hashedPassword);
+//     req.session.user_id = uniqueId;
+//     res.redirect('/urls');
+//   }
+// }
+
+module.exports = { validateLogIn };
