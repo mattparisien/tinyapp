5e0edfd1da8e9c237db674c1dd2d78f6d9936df2
Matthew Parisien <matthewparisien4@gmail.com>
2021-09-01 19:56:49 +0000

    Add missing 400 response statuses and redirect to get request instead of rendering error from post request.

--- validateForms.js
+++ validateForms.js
@@ -4,13 +4,11 @@ const { fetchUserByEmail, fetchPassword } = require('./helpers');
 
 const validateLogIn = function(object, email, password, req, res, bcrypt) {
   if (!req.body.email || !req.body.password) {
-    const templateVars = { validationError: "Please fill out the fields." } ;
-    res.status(400).render('login', templateVars);
-    return false;
+    res.status(400).redirect(`login?error=${'Please fill out the fields.'}`)
   
     //If no user ID exists, send error
   } else if (!fetchUserByEmail(object, email)) {
-    res.redirect(`/login?error=${"Email is not registered."}`);
+    res.status(400).redirect(`/login?error=${"Email is not registered."}`);
   
   //If unhashed, req.body password does not match hashed database password, send error
   } else if (!bcrypt.compareSync(password, fetchPassword(object, fetchUserByEmail(object, email)['id']))) {
